d6aa16066886382d9c20403e51872d7b569323a9
FeroxFoxxo <feroxfoxxo@gmail.com>
2023-02-01 09:37:28 +1300

    Added character delete protocol

---
+++ Protocols.External/[c] CharacterInfoHandler/[d] DeleteCharacter.cs
@@ -0,0 +1,26 @@
+﻿using Server.Reawakened.Core.Network.Protocols;
+using Server.Reawakened.Players;
+
+namespace Protocols.External._c__CharacterInfoHandler;
+
+public class DeleteCharacter : ExternalProtocol
+{
+    public override string ProtocolName => "cd";
+
+    public override void Run(string[] message)
+    {
+        var player = NetState.Get<Player>();
+
+        var characterName = message[5];
+
+        var character = player.UserInfo.Characters
+            .FirstOrDefault(c => c.Value.CharacterName == characterName);
+
+        var characterExists = character.Value != null;
+
+        if (characterExists)
+            player.UserInfo.Characters.Remove(character.Key);
+         
+        SendXt("cd", characterExists ? 0 : 1);
+    }
+}
