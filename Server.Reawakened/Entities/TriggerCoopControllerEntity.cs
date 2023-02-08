a6167590ec355133df1a96ee0c8eb0492b4bae4c
DaLNakii <souleimene.usma@yahoo.fr>
2023-02-08 16:25:49 +0100

    trigger impl attempt

---
+++ Server.Reawakened/Entities/TriggerCoopControllerEntity.cs
@@ -0,0 +1,37 @@
+﻿using Server.Base.Network;
+using Server.Reawakened.Levels.Models.Entities;
+using Server.Reawakened.Players;
+using Server.Reawakened.Players.Extensions;
+
+namespace Server.Reawakened.Entities;
+
+internal class TriggerCoopControllerEntity : SyncedEntity<TriggerCoopController>
+{
+    public bool Activated = false;
+
+    public override string[] GetInitData(NetState netState) => new[] { Activated ? "1" : "0" };
+
+    public override void RunSyncedEvent(SyncEvent syncEvent, NetState netState)
+    {
+        var player = netState.Get<Player>();
+
+        Level.SendSyncEvent(syncEvent);
+        netState.SendSyncEventToPlayer(syncEvent);
+
+        Trigger_SyncEvent trigEvent;
+
+        if (EntityData.TargetLevelEditorID != 0 && EntityData.TargetToDeactivateLevelEditorID == 0)
+        {
+            trigEvent = new Trigger_SyncEvent(EntityData.TargetLevelEditorID.ToString(), Level.Time, true, player.PlayerId.ToString(), true);
+            Activated = true;
+        }
+        else
+        {
+            Activated = false;
+            trigEvent = new Trigger_SyncEvent(EntityData.TargetToDeactivateLevelEditorID.ToString(), Level.Time, true, player.PlayerId.ToString(), false);
+        }
+
+        Level.SendSyncEvent(trigEvent);
+        netState.SendSyncEventToPlayer(trigEvent);
+    }
+}
