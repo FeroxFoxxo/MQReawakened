08da5737362c08e4bcea16ac0884e8f0d95da00d
FeroxFoxxo <feroxfoxxo@gmail.com>
2023-03-03 00:24:15 +1300

    Temporary bypass for chat

---
+++ Web.Apps/Chat/Services/AddChatBypass.cs
@@ -0,0 +1,13 @@
+﻿using Server.Base.Core.Abstractions;
+using Server.Web.Models;
+
+namespace Web.Apps.Chat.Services;
+
+public class AddChatBypass : IService
+{
+    private readonly WebRConfig _config;
+
+    public AddChatBypass(WebRConfig config) => _config = config;
+
+    public void Initialize() => _config.IgnorePaths.Add("/Chat/");
+}
