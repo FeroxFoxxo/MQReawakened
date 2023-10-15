e183b7f31039e36884cd4c072c23391ee295a1ab
david <davidawesomea@gmail.com>
2023-10-15 12:48:34 -0600

    Add CannedChat XML loader, add CannedChat protocol

---
+++ Protocols.External/[a] ChatHandler/[p] CannedChat.cs
@@ -0,0 +1,33 @@
+﻿using A2m.Server;
+using Microsoft.Extensions.Logging;
+using Server.Reawakened.Chat.Services;
+using Server.Reawakened.Configs;
+using Server.Reawakened.Network.Extensions;
+using Server.Reawakened.Network.Protocols;
+using Server.Reawakened.Players.Services;
+using Server.Reawakened.XMLs.Bundles;
+using static LeaderBoardTopScoresJson;
+
+namespace Protocols.External._a__ChatHandler;
+
+public class CannedChat : ExternalProtocol
+{
+    public override string ProtocolName => "ap";
+
+    public ILogger<ChatCommands> Logger { get; set; }
+    public ChatCommands ChatCommands { get; set; }
+    public ServerRConfig Config { get; set; }
+    public CannedChatDictionary CannedChatDict { get; set; }
+
+    public override void Run(string[] message)
+    {
+        var channelType = (CannedChatChannel)Convert.ToInt32(message[5]);
+        var chatPhraseId = Convert.ToInt32(message[6]);
+        var recipientName = message[7];
+
+        var character = Player.Character;
+        var chatPhrase = CannedChatDict.GetDialogById(chatPhraseId);
+
+        Player.Room.Chat(channelType, character.Data.CharacterName, chatPhrase);
+    }
+}
---
+++ Server.Reawakened/XMLs/Bundles/CannedChatDictionary.cs
@@ -0,0 +1,80 @@
+﻿using A2m.Server;
+using Server.Base.Core.Extensions;
+using Server.Reawakened.XMLs.Abstractions;
+using Server.Reawakened.XMLs.Enums;
+using System.Xml;
+
+namespace Server.Reawakened.XMLs.Bundles;
+
+public class CannedChatDictionary : CannedChatXML, ILocalizationXml
+{
+    public string BundleName => "CannedChat";
+    public string LocalizationName => "CannedChatDict_en-US";
+    public BundlePriority Priority => BundlePriority.Low;
+    public Microsoft.Extensions.Logging.ILogger Logger { get; set; }
+    public IServiceProvider Services { get; set; }
+
+    private Dictionary<int, string> CannedChatDict;
+
+    public void InitializeVariables()
+    {
+        CannedChatDict = [];
+
+        this.SetField<CannedChatXML>("_cannedChatDict", new Dictionary<int, CategoryNode>());
+        this.SetField<CannedChatXML>("_dic", new Dictionary<int, string>());
+    }
+
+    public void EditLocalization(XmlDocument xml)
+    {
+    }
+
+    public void ReadLocalization(string xml)
+    {
+        var xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(xml);
+
+        foreach (XmlNode phraseRoot in xmlDoc.ChildNodes)
+        {
+            if (!(phraseRoot.Name == "CannedChatDict"))
+            {
+                continue;
+            }
+
+            foreach (XmlNode phrase in phraseRoot.ChildNodes)
+            {
+                var id = -1;
+
+                foreach (XmlAttribute attribute in phrase.Attributes)
+                {
+                    if (attribute.Name == "id")
+                        id = int.Parse(attribute.Value);
+                }
+                var text = phrase.InnerText;
+
+                CannedChatDict.Add(id, text);
+            }
+        }
+    }
+
+    public void EditDescription(XmlDocument xml)
+    {
+    }
+
+    public void ReadDescription(string xml) => ReadDescriptionXml(xml);
+
+    public void FinalizeBundle()
+    {
+    }
+
+    public string GetDialogById(int id) =>
+        CannedChatDict.TryGetValue(id, out var text) ? text : null;
+
+    public void PrintDictionary()
+    {
+        Console.WriteLine($"CannedChatPhrases Dictionary: (size: {CannedChatDict.Count})");
+        foreach (KeyValuePair<int, string> kvp in CannedChatDict)
+        {
+            Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+        }
+    }
+}
