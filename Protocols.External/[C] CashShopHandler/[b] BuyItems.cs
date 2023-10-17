f2423cb3860aab9d494898a5a31576e4635619f5
Draykos <drake.e.bass0830@gmail.com>
2023-10-16 23:04:20 -0400

    Add TravelToFriend/Group, update BuyItems for CashShopHandler, Add GiftItemShop

---
+++ Protocols.External/[C] CashShopHandler/[b] BuyItems.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using Server.Base.Accounts.Extensions;
+using Server.Reawakened.Network.Protocols;
+using Server.Reawakened.Players;
+using Server.Reawakened.Players.Extensions;
+using Server.Reawakened.XMLs.Bundles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protocols.External._C__CashShopHandler;
+public class BuyItems : ExternalProtocol
+{
+    public override string ProtocolName => "Cb";
+
+    public ItemCatalog ItemCatalog { get; set; }
+
+    public override void Run(string[] message)
+    {
+        var character = Player.Character;
+
+        //var vendorId = int.Parse(message[5]);
+        //var vendorGoId = int.Parse(message[7]);
+        var items = message[6].Split('|');
+        foreach (var item in items)
+        {
+            if (string.IsNullOrEmpty(item)) continue;
+
+            var args = item.Split(":");
+            var itemId = int.Parse(args[0]);
+            var amount = int.Parse(args[1]);
+            character.AddItem(ItemCatalog.GetItemFromId(itemId), amount);
+            var itemInfo = ItemCatalog.GetItemFromId(itemId);
+            Player.AddNCash(-itemInfo.RegularPrice * amount);
+        }
+        Player.SendCashUpdate();
+        Player.SendUpdatedInventory(false);
+
+    }
+}
---
+++ Protocols.External/[C] CashShopHandler/[g] GiftItemShop.cs
@@ -0,0 +1,52 @@
+﻿using A2m.Server;
+using A2m.Server.Messages;
+using Server.Reawakened.Network.Extensions;
+using Server.Reawakened.Network.Protocols;
+using Server.Reawakened.Players;
+using Server.Reawakened.Players.Extensions;
+using Server.Reawakened.Players.Helpers;
+using Server.Reawakened.Players.Models;
+using Server.Reawakened.Players.Models.Character;
+using Server.Reawakened.Players.Models.System;
+using Server.Reawakened.XMLs.Bundles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift.Protocol;
+
+namespace Protocols.External._C__CashShopHandler;
+public class GiftItemShop : ExternalProtocol
+{
+    public override string ProtocolName => "Cg";
+
+    public ItemCatalog ItemCatalog { get; set; }
+
+    public PlayerHandler PlayerHandler { get; set; }
+
+    public override void Run(string[] message)
+    {
+        var character = Player.Character;
+
+        var otherPlayerName = message[6];
+        var messageId = int.Parse(message[7]);
+        var itemId = int.Parse(message[8]);
+        var cardId = int.Parse(message[9]);
+        var boxId = int.Parse(message[10]);
+        var itemDescription = ItemCatalog.GetItemFromId(itemId);
+
+        var boxDescription = ItemCatalog.GetItemFromId(boxId);
+        var boxPrice = boxDescription.RegularPrice;
+        Player.AddBananas(-boxPrice);
+
+        var otherPlayer = PlayerHandler.PlayerList
+            .FirstOrDefault(p => p.Character.Data.CharacterName == otherPlayerName);
+
+        otherPlayer.Character.AddItem(itemDescription, 1);
+
+        otherPlayer.SendUpdatedInventory(false);
+
+        otherPlayer?.SendXt("Cg", 1, otherPlayerName, messageId, itemId, cardId, boxId);
+    }
+}
