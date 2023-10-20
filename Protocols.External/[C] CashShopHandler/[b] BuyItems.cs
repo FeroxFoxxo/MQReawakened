45263620eb99a4a1aa3b198d025f38eaa431c6a1
FeroxFoxxo <feroxfoxxo@gmail.com>
2023-10-21 07:57:07 +1300

    Added comments that gifting and travelling isnt fully supported yet

--- Protocols.External/[C] CashShopHandler/[b] BuyItems.cs
+++ Protocols.External/[C] CashShopHandler/[b] BuyItems.cs
@@ -1,22 +1,33 @@
-﻿using Server.Reawakened.Network.Protocols;
+﻿using A2m.Server;
+using Microsoft.Extensions.Logging;
+using Server.Reawakened.Network.Protocols;
 using Server.Reawakened.Players;
 using Server.Reawakened.Players.Extensions;
 using Server.Reawakened.XMLs.Bundles;
 
 namespace Protocols.External._C__CashShopHandler;
+
 public class BuyItems : ExternalProtocol
 {
     public override string ProtocolName => "Cb";
 
     public ItemCatalog ItemCatalog { get; set; }
+    public ILogger<BuyItems> Logger { get; set; }
 
     public override void Run(string[] message)
     {
         var character = Player.Character;
 
-        //var vendorId = int.Parse(message[5]);
-        //var vendorGoId = int.Parse(message[7]);
+        var cashShop = (Cashshop) int.Parse(message[5]);
+
+        if (cashShop != Cashshop.CashShop)
+        {
+            Logger.LogWarning("Unknown cashshop of type {Type}!", cashShop);
+            return;
+        }
+
         var items = message[6].Split('|');
+
         foreach (var item in items)
         {
             if (string.IsNullOrEmpty(item)) continue;
