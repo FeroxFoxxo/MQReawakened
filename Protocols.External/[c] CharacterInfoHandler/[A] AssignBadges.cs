fedcbf1edfa42b26ebd1570d5f926a112d0f0d83
Draykos <drake.e.bass0830@gmail.com>
2023-10-31 06:16:04 -0400

    Add AssignBadges protocol, add PromoteMember protocol, minor update for ChestController, add new items, add new vendor

--- Protocols.External/[c] CharacterInfoHandler/[A] AssignBadges.cs
+++ Protocols.External/[c] CharacterInfoHandler/[A] AssignBadges.cs
@@ -1,17 +1,8 @@
 ﻿using A2m.Server;
-using Newtonsoft.Json.Linq;
-using Server.Base;
-using Server.Reawakened.Network.Extensions;
 using Server.Reawakened.Network.Protocols;
 using Server.Reawakened.Players;
-using Server.Reawakened.Players.Extensions;
 using Server.Reawakened.Players.Helpers;
 using Server.Reawakened.Players.Models.Character;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 
 namespace Protocols.External._c__CharacterInfoHandler
 {
@@ -19,39 +10,49 @@ namespace Protocols.External._c__CharacterInfoHandler
     {
         public override string ProtocolName => "cA";
 
-        public Dictionary<TribeType, TribeDataModel> tribesProgression;
-
         public override void Run(string[] message)
         {
             var tribeDataList = new List<Dictionary<TribeType, TribeDataModel>>();
+            var tribeProgressionData = new Dictionary<TribeType, TribeDataModel>();
 
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("--===========--");
-            foreach (var msg in message)
-                Console.WriteLine(msg);
-            Console.WriteLine("--===========--");
-
-            var charData = Player.Character.Data;
-
-            foreach (var tribes in message.Skip(5))
+            foreach (var tribe in message.Skip(5))
             {
-                var tribeData = new TribeDataModel(tribes);
+                if (string.IsNullOrEmpty(tribe)) continue;
 
-                string GenerateTribeData(Dictionary<TribeType, TribeDataModel> dataList)
+                var tribeData = tribe.Split('|');
+                var tribeType = (TribeType)Enum.Parse(typeof(TribeType), tribeData[0]);
+                var pointAmount = int.Parse(tribeData[1]);
+                var unlocked = tribeData[2] == "1";
+
+                var tribeTypeModel = new TribeDataModel()
                 {
-                    var sb = new SeparatedStringBuilder('<');
+                    TribeType = tribeType,
+                    BadgePoints = pointAmount,
+                    Unlocked = unlocked
+                };
+                tribeProgressionData.Add(tribeType, tribeTypeModel);
 
-                    foreach (var tribes in dataList)
-                        sb.Append(tribes);
-
-                    tribeDataList.Add(dataList);
-                    return sb.ToString();
-                }
-
-                SendXt("cA", tribeDataList, (int)charData.Allegiance, 1);
-
+                tribeDataList.Add(tribeProgressionData);
             }
 
+            var autoAssign = 0;
+            if (Player.Character.Data.TribesProgression.Values.Count % 5 == 0)
+                autoAssign = 1;
+
+            Player.Character.Data.TribesProgression = tribeProgressionData;
+
+            SendXt("cA", GenerateTribeData(tribeProgressionData),
+                (int)Player.Character.Data.Allegiance, autoAssign);
+        }
+
+        private string GenerateTribeData(Dictionary<TribeType, TribeDataModel> dataList)
+        {
+            var sb = new SeparatedStringBuilder('<');
+
+            foreach (var tribeData in dataList)
+                sb.Append($"{(int)tribeData.Key}|{tribeData.Value.BadgePoints}|{(tribeData.Value.Unlocked ? 1 : 0)}");
+
+            return sb.ToString();
         }
     }
 }
--- Server.Reawakened/Entities/ChestControllerEntity.cs
+++ Server.Reawakened/Entities/ChestControllerEntity.cs
@@ -2,6 +2,7 @@
 using Server.Reawakened.Players;
 using Server.Reawakened.Players.Extensions;
 using Server.Reawakened.Rooms.Extensions;
+using Server.Reawakened.XMLs.Bundles;
 
 namespace Server.Reawakened.Entities;
 
@@ -10,6 +11,8 @@ public class ChestControllerEntity : AbstractBaseChest<ChestController>
     public bool Collected;
 
     public Random Random { get; set; }
+
+    public ItemCatalog ItemCatalog { get; set; }
 
     public override object[] GetInitData(Player player) => new object[] { Collected ? 0 : 1 };
 
@@ -20,8 +23,32 @@ public class ChestControllerEntity : AbstractBaseChest<ChestController>
 
         Collected = true;
 
-        var bananas = Random.Next(10, 100);
+        var bananas = Random.Next(50, 101);
         player.AddBananas(bananas);
+
+        var rdmItem = Random.Next(1, 7);
+        switch (rdmItem)
+        {
+            case 1:
+                player.Character.AddItem(ItemCatalog.GetItemFromId(1568), Random.Next(1, 4));
+                break;
+            case 2:
+                player.Character.AddItem(ItemCatalog.GetItemFromId(1803), 1);
+                break;
+            case 3:
+                player.Character.AddItem(ItemCatalog.GetItemFromId(404), 1);
+                break;
+            case 4:
+                player.Character.AddItem(ItemCatalog.GetItemFromId(1070), 1);
+                break;
+            case 5:
+                player.Character.AddItem(ItemCatalog.GetItemFromId(2871), 1);
+                break;
+            case 6:
+                player.Character.AddItem(ItemCatalog.GetItemFromId(1576), 1);
+                break;
+        }
+        player.SendUpdatedInventory(false);
 
         var trig = new Trigger_SyncEvent(Id.ToString(), Room.Time, true, player.GameObjectId.ToString(), true)
         {
