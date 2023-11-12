36147523a83332ddc28d1338e17d283b996dc799
david <davidawesomea@gmail.com>
2023-11-11 22:40:42 -0700

    Add PlayerDamageExtensions class, update Collision syncevent

--- Server.Reawakened/Entities/HazardControllerEntity.cs
+++ Server.Reawakened/Entities/HazardControllerEntity.cs
@@ -1,6 +1,7 @@
 ﻿using A2m.Server;
 using Microsoft.Extensions.Logging;
 using Server.Reawakened.Players;
+using Server.Reawakened.Players.Extensions;
 using Server.Reawakened.Rooms.Extensions;
 using Server.Reawakened.Rooms.Models.Entities;
 
@@ -27,15 +28,22 @@ public class HazardControllerEntity : SyncedEntity<HazardController>
 
     public override void NotifyCollision(NotifyCollision_SyncEvent notifyCollisionEvent, Player player)
     {
+        /* seems redundant with the other return statement below? not sure though
         if (HurtEffect == "NoEffect")
+        {
             return;
-
-        var character = player.Character;
+        }
+        */
 
         Enum.TryParse(HurtEffect, true, out ItemEffectType effectType);
 
+        var character = player.Character;
+
         if (effectType == default)
         {
+            if (notifyCollisionEvent.Colliding && notifyCollisionEvent.Message == "HitDamageZone") //probably won't work for until some collisions failing is fixed
+                player.ApplyDamageByObject(Room, int.Parse(notifyCollisionEvent.CollisionTarget));
+
             Logger.LogWarning("No hazard type found for {Type}. Returning...", HurtEffect);
             return;
         }
@@ -50,6 +58,9 @@ public class HazardControllerEntity : SyncedEntity<HazardController>
 
         switch (effectType)
         {
+            case ItemEffectType.FireDamage:
+                player.ApplyDamageByPercent(Room, .10);
+                break;
             default:
                 SendEntityMethodUnknown("unran-hazards", "Failed Hazard Event", "Hazard Type Switch",
                     $"Effect Type: {effectType}");
