966f80b1b9bf8f3d532ca67d0c94a6bbf9203796
Draykos <drake.e.bass0830@gmail.com>
2023-12-23 18:45:02 -0500

    for ferox

---
+++ Protocols.External/[M] MinigameHandler/[m] FinishMinigame.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Extensions.Logging;
+using RaceDefines;
+using Server.Reawakened.Network.Extensions;
+using Server.Reawakened.Network.Protocols;
+using Server.Reawakened.Players;
+using Server.Reawakened.Players.Extensions;
+using Server.Reawakened.Players.Helpers;
+using Server.Reawakened.Rooms.Enums;
+using Server.Reawakened.Rooms.Extensions;
+using Server.Reawakened.XMLs.Bundles;
+using Server.Reawakened.XMLs.BundlesInternal;
+using Server.Reawakened.XMLs.Models.LootRewards;
+using static Analytics;
+
+namespace Protocols.External._M__MinigameHandler;
+public class FinishedMinigame : ExternalProtocol
+{
+    public override string ProtocolName => "Mm";
+
+    public LootCatalogInt LootCatalog { get; set; }
+
+    public ItemCatalog ItemCatalog { get; set; }
+
+    public MinigameCatalogInt MinigameCatalog { get; set; }
+
+    public ILogger<FinishedMinigame> Logger { get; set; }
+
+    public override void Run(string[] message)
+    {
+        var minigameId = int.Parse(message[5]);
+        var finishedRaceTime = float.Parse(message[6]);
+
+        Logger.LogInformation("Minigame with ID ({minigameId}) has completed.", minigameId);
+
+        SendXt("Mt", minigameId, Player.GameObjectId, finishedRaceTime);
+
+        if (Player.TempData.ArenaModel.BestTimeForLevel == null)
+        {
+            Player.TempData.ArenaModel.BestTimeForLevel = [];
+            Player.TempData.ArenaModel.BestTimeForLevel.Add(Player.Room.LevelInfo.Name, finishedRaceTime);
+        }
+
+        if (finishedRaceTime < Player.TempData.ArenaModel.BestTimeForLevel[Player.Room.LevelInfo.Name])
+        {
+            //Store best time in dictionary for timed minigame.
+            Player.TempData.ArenaModel.BestTimeForLevel[Player.Room.LevelInfo.Name] = finishedRaceTime;
+            Player.SendXt("Ms", Player.Room.LevelInfo.InGameName);
+        }
+
+        Player.TempData.ArenaModel.StartArena = false;
+        Player.TempData.ArenaModel.HasStarted = false;
+
+        if (Player.TempData.Group != null)
+        {
+            var groupMember = Player.TempData.Group.GetMembers();
+
+            if (groupMember.All(p => !p.TempData.ArenaModel.HasStarted))
+                foreach (var member in groupMember)
+                    FinishMinigame(minigameId, groupMember.Count);
+        }
+
+        else
+            FinishMinigame(minigameId, 1);
+    }
+
+    public void FinishMinigame(int minigameId, int membersInGroup)
+    {
+        var endRace = new TriggerUpdate_SyncEvent(minigameId.ToString(), Player.Room.Time, membersInGroup);
+        Player.Room.SendSyncEvent(endRace);
+
+        var dataList = new List<string>();
+
+        var rdmBananaReward = new Random().Next(7, 12 * Player.Character.Data.GlobalLevel);
+        var xpReward = Player.Character.Data.ReputationForNextLevel / 11;
+
+        var lootableItems = Player.GrantLootableItems(LootCatalog, minigameId);
+        var lootedItems = Player.GrantItemsLooted(LootCatalog, minigameId);
+
+        dataList.Add(membersInGroup.ToString());
+        dataList.Add(rdmBananaReward.ToString());
+        dataList.Add(xpReward.ToString());
+        dataList.Add(lootedItems.ToString());
+        dataList.Add(lootableItems.ToString());
+
+        SendXt("Mp", minigameId, SplitRewardData(dataList));
+
+        Player.SendCashUpdate();
+    }
+
+    private string SplitRewardData(List<string> dataList)
+    {
+        var sb = new SeparatedStringBuilder('<');
+
+        foreach (var data in dataList)
+            sb.Append(data.ToString());
+
+        return sb.ToString();
+    }
+
+}
---
+++ Server.Reawakened/Entities/Components/TriggerCoopArenaSwitchControllerComp.cs
@@ -0,0 +1,63 @@
+﻿using A2m.Server;
+using Microsoft.Extensions.Logging;
+using Server.Base.Logging;
+using Server.Reawakened.Network.Extensions;
+using Server.Reawakened.Players;
+using Server.Reawakened.Players.Extensions;
+using Server.Reawakened.Players.Helpers;
+using Server.Reawakened.Players.Models.Character;
+using Server.Reawakened.Rooms.Extensions;
+using Server.Reawakened.Rooms.Models.Entities;
+using Server.Reawakened.XMLs.BundlesInternal;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Analytics;
+
+namespace Server.Reawakened.Entities.Components;
+public class TriggerCoopArenaSwitchControllerComp : Component<TriggerCoopArenaSwitchController>
+{
+    public string ArenaObjectId => ComponentData.ArenaObjectID;
+    public MinigameCatalogInt MinigameCatalogInt { get; set; }
+    public ILogger<TriggerCoopArenaSwitchControllerComp> Logger { get; set; }
+
+    public override object[] GetInitData(Player player) => base.GetInitData(player);
+
+    public override void RunSyncedEvent(SyncEvent syncEvent, Player player)
+    {
+        if (player.TempData.ArenaModel.HasStarted)
+        {
+            Logger.LogInformation("Arena has already started, stopping syncEvent.");
+            player.TempData.ArenaModel.StartArena = true;
+            return;
+        }
+
+        player.Room.SendSyncEvent(syncEvent);
+
+        var playersInRoom = Convert.ToInt32(syncEvent.EventDataList[1]);
+        var arenaActivation = Convert.ToInt32(syncEvent.EventDataList[2]);
+
+        //Method to determine if arena trigger event is minigame. if minigame, proceed with code below.
+
+        player.TempData.ArenaModel.ArenaObjectId = int.Parse(ArenaObjectId);
+        player.TempData.ArenaModel.StartArena = arenaActivation > 0;
+
+        if (player.TempData.Group != null)
+        {
+            var playersInGroup = player.TempData.Group.GetMembers();
+            if (playersInGroup.All(p => p.TempData.ArenaModel.StartArena))
+            {
+                foreach (var member in playersInGroup)
+                {
+                    member.TempData.ArenaModel.SetCharacterIds(member, playersInGroup);
+                    member.TempData.ArenaModel.HasStarted = true;
+                }
+                var startRace = new Trigger_SyncEvent(ArenaObjectId, Room.Time, true, player.GameObjectId.ToString(), Room.LevelInfo.LevelId, true, true);
+                Room.SendSyncEvent(startRace);
+            }
+        }
+    }
+}
