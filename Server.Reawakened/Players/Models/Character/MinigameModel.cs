12c30912aa4b6c7597c631f7b967c0089d7064ac
Draykos <drake.e.bass0830@gmail.com>
2023-12-24 18:35:04 -0500

    ArenaModel

---
+++ Server.Reawakened/Players/Models/Character/MinigameModel.cs
@@ -0,0 +1,27 @@
+﻿using Server.Reawakened.Entities.Components;
+using Server.Reawakened.Players.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Reawakened.Players.Models.Character;
+public class ArenaModel
+{
+    public bool StartArena { get; set; }
+    public int FirstPlayerId { get; set; }
+    public int SecondPlayerId { get; set; }
+    public int ThirdPlayerId { get; set; }
+    public int FourthPlayerId { get; set; }
+    public Dictionary<string, float> BestTimeForLevel { get; set; }
+
+    public void SetCharacterIds(Player player, IEnumerable<Player> players)
+    {
+        var playersInGroup = players.ToArray();
+        player.TempData.ArenaModel.FirstPlayerId = playersInGroup.Length > 0 ? playersInGroup[0].GameObjectId : 0;
+        player.TempData.ArenaModel.SecondPlayerId = playersInGroup.Length > 1 ? playersInGroup[1].GameObjectId : 0;
+        player.TempData.ArenaModel.ThirdPlayerId = playersInGroup.Length > 2 ? playersInGroup[2].GameObjectId : 0;
+        player.TempData.ArenaModel.FourthPlayerId = playersInGroup.Length > 3 ? playersInGroup[3].GameObjectId : 0;
+    }
+}
