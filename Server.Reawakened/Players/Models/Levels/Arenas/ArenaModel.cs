171357a5d2feeb645e7a3a39b3de283bf290ed1e
Draykos <drake.e.bass0830@gmail.com>
2024-01-13 20:58:57 -0500

    healing code revamp (wip)

---
+++ Server.Reawakened/Players/Models/Levels/Arenas/ArenaModel.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Extensions.Logging;
+using Server.Reawakened.Network.Extensions;
+using Server.Reawakened.Network.Protocols;
+using Server.Reawakened.Players;
+using Server.Reawakened.Players.Extensions;
+using Server.Reawakened.Players.Helpers;
+using Server.Reawakened.Players.Models.Minigames;
+using Server.Reawakened.Players.Models.Character;
+using Server.Reawakened.Rooms.Extensions;
+using Server.Reawakened.XMLs.BundlesInternal;
+
+namespace Server.Reawakened.Players.Models.Minigames;
+public class ArenaModel
+{
+    public bool IsMinigame { get; set; }
+    public int MinigameTimeLimit { get; set; }
+    public bool ActivatedSwitch { get; set; }
+    public bool ArenaActivated { get; set; }
+    public int FirstPlayerId { get; set; }
+    public int SecondPlayerId { get; set; }
+    public int ThirdPlayerId { get; set; }
+    public int FourthPlayerId { get; set; }
+    public Dictionary<string, float> BestTimeForLevel { get; set; } = [];
+    public List<Player> Participants { get; set; } = [];
+    public InternalLoot LootCatalog { get; set; }
+
+    public void SetCharacterIds(IEnumerable<Player> players)
+    {
+        var playersInRoom = players.ToArray();
+
+        FirstPlayerId = playersInRoom.Length > 0 ? playersInRoom[0].CharacterId : 0;
+        SecondPlayerId = playersInRoom.Length > 1 ? playersInRoom[1].CharacterId : 0;
+        ThirdPlayerId = playersInRoom.Length > 2 ? playersInRoom[2].CharacterId : 0;
+        FourthPlayerId = playersInRoom.Length > 3 ? playersInRoom[3].CharacterId : 0;
+    }
+
+    public void FinishMinigame(int minigameId, Player player)
+    {
+        ArenaActivated = false;
+        var playersInRace = Participants;
+
+        var rdmBananaReward = new Random().Next(7, 11) * player.Character.Data.GlobalLevel;
+        var xpReward = player.Character.Data.ReputationForNextLevel / 11;
+
+        var lootedItems = GrantRandomItemLoot(LootCatalog, minigameId);
+        var lootableItems = GrantLootableItems(LootCatalog, minigameId);
+
+        var sb = new SeparatedStringBuilder('<');
+
+        sb.Append(playersInRace.Count);
+        sb.Append(rdmBananaReward);
+        sb.Append(xpReward);
+        sb.Append(lootedItems);
+        sb.Append(lootableItems);
+
+        foreach (var participant in playersInRace)
+        {
+            participant.SendXt("Mp", minigameId, sb.ToString());
+            participant.SendSyncEventToPlayer(new TriggerUpdate_SyncEvent(minigameId.ToString(), participant.Room.Time, playersInRace.Count));
+
+            participant.SendCashUpdate();
+            participant.SendUpdatedInventory(false);
+        }
+    }
+
+    public static string GrantRandomItemLoot(InternalLoot lootCatalog, int arenaId)
+    {
+        var random = new Random();
+        var itemsGotten = new List<ItemModel>();
+
+        foreach (var reward in lootCatalog.LootCatalog[arenaId].ItemRewards)
+            foreach (var item in reward.Items)
+                itemsGotten.Add(item);
+
+        var randomItemRewardLoot = itemsGotten[random.Next(itemsGotten.Count)];
+
+        return FormatItemString(randomItemRewardLoot);
+    }
+
+    public static string GrantLootableItems(InternalLoot LootCatalog, int arenaId)
+    {
+        var lootableItems = new SeparatedStringBuilder('|');
+
+        if (LootCatalog.LootCatalog.TryGetValue(arenaId, out var value))
+            foreach (var reward in value.ItemRewards)
+                foreach (var itemReward in reward.Items)
+                    lootableItems.Append(itemReward.ItemId);
+
+        return lootableItems.ToString();
+    }
+
+    public static string FormatItemString(ItemModel item)
+    {
+        var sb = new SeparatedStringBuilder('{');
+
+        sb.Append(item.ItemId);
+        sb.Append(item.Count);
+        sb.Append(item.BindingCount);
+        sb.Append(DateTime.Now);
+
+        return sb.ToString();
+    }
+}
---
+++ Server.Reawakened/XMLs/BundlesInternal/InternalArena.cs
@@ -0,0 +1,82 @@
+﻿using A2m.Server;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Server.Reawakened.Players.Models.Minigames;
+using Server.Reawakened.XMLs.Abstractions;
+using Server.Reawakened.XMLs.Bundles;
+using Server.Reawakened.XMLs.Enums;
+using System.Xml;
+
+namespace Server.Reawakened.XMLs.BundlesInternal;
+
+public class InternalArena : IBundledXml<InternalArena>
+{
+    public string BundleName => "InternalArena";
+    public BundlePriority Priority => BundlePriority.Lowest;
+    public IServiceProvider Services { get; set; }
+    public ILogger<InternalArena> Logger { get; set; }
+
+    public Dictionary<int, ArenaModel> Arena;
+
+    public InternalArena()
+    {
+    }
+
+    public void InitializeVariables() =>
+        Arena = [];
+
+    public void EditDescription(XmlDocument xml)
+    {
+    }
+
+    public void ReadDescription(string xml)
+    {
+        var xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(xml);
+
+        foreach (XmlNode arenaCatalog in xmlDocument.ChildNodes)
+        {
+            if (!(arenaCatalog.Name == "ArenaCatalogs")) continue;
+
+            foreach (XmlNode level in arenaCatalog.ChildNodes)
+            {
+                if (!(level.Name == "LevelName")) continue;
+
+                var arenaId = -1;
+                var timeLimit = -1;
+                var isMinigame = false;
+
+                foreach (XmlNode arenaInformation in level.ChildNodes)
+                {
+                    if (!(arenaInformation.Name == "ArenaInformation")) continue;
+
+                    foreach (XmlAttribute arenaAttribute in arenaInformation.Attributes)
+                    switch (arenaAttribute.Name)
+                    {
+                        case "arenaId":
+                            arenaId = int.Parse(arenaAttribute.Value);
+                            break;
+                        case "timeLimit":
+                            timeLimit = int.Parse(arenaAttribute.Value);
+                            break;
+                        case "isMinigame":
+                            isMinigame = bool.Parse(arenaAttribute.Value);
+                            break;
+                    }
+
+                    var arenaModel = new ArenaModel()
+                    {
+                        IsMinigame = isMinigame,
+                        MinigameTimeLimit = timeLimit
+                    };
+
+                    Arena.Add(arenaId, arenaModel);
+                }
+            }
+        }
+    }
+
+    public void FinalizeBundle()
+    {
+    }
+}
