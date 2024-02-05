ee6efd27ec13b645ea818459c1ee65d4a0a86368
FeroxFoxxo <feroxfoxxo@gmail.com>
2024-02-05 13:32:21 +1300

    Added log for cutscene

--- Protocols.External/[M] MinigameHandler/[f] FinishCutscene.cs
+++ Protocols.External/[M] MinigameHandler/[f] FinishCutscene.cs
@@ -1,33 +1,37 @@
-﻿using Server.Reawakened.Entities.Components;
+﻿using Microsoft.Extensions.Logging;
+using Server.Reawakened.Entities.Components;
 using Server.Reawakened.Network.Extensions;
 using Server.Reawakened.Network.Protocols;
 using Server.Reawakened.Players;
 using Server.Reawakened.Players.Helpers;
-using Server.Reawakened.Rooms;
 
 namespace Protocols.External._M__MinigameHandler;
 
 public class FinishCutscene : ExternalProtocol
 {
     public override string ProtocolName => "Mf";
+    public ILogger<FinishCutscene> Logger { get; set; }
 
     public override void Run(string[] message)
     {
         var arenaObjectId = message[5];
 
-        TriggerArenaComp arena = null;
+        TriggerCoopControllerComp trigger = null;
 
         if (Player.Room.Entities.TryGetValue(arenaObjectId, out var foundEntity))
             foreach (var component in foundEntity)
-                if (component is TriggerArenaComp arenaComponent)
-                    arena = arenaComponent;
+                if (component is TriggerCoopControllerComp foundTrigger)
+                    trigger = foundTrigger;
 
-        if (arena == null)
+        if (trigger == null)
+        {
+            Logger.LogError("Could not find cutscene finish trigger for {Id}", arenaObjectId);
             return;
+        }
 
         var players = new SeparatedStringBuilder('%');
 
-        foreach (var playerId in arena.GetPhysicalInteractorIds())
+        foreach (var playerId in trigger.GetPhysicalInteractorIds())
             players.Append(playerId);
 
         Player.SendXt("Mc", arenaObjectId, Player.Room.Time, 5f, players.ToString());
--- Server.Reawakened/Entities/Components/TriggerRaceComp.cs
+++ Server.Reawakened/Entities/Components/TriggerRaceComp.cs
@@ -9,8 +9,31 @@ public class TriggerRaceComp : TriggerStatueComp<TriggerRace>
     public string[] TargetToEnableDelayedLevelEditorIDs => ComponentData.TargetToEnableDelayedLevelEditorIDs;
     public string[] TargetToResetLevelEditorIDs => ComponentData.TargetToResetLevelEditorIDs;
 
+    public bool IsMinigameStarted;
+    public bool HasMinigameStartedBefore;
+
+    public override object[] GetInitData(Player player) => [IsMinigameStarted ? 1 : 0, HasMinigameStartedBefore ? 1 : 0];
+
+    public override void InitializeComponent()
+    {
+        IsMinigameStarted = false;
+        HasMinigameStartedBefore = false;
+        base.InitializeComponent();
+    }
+
     public override void Triggered(Player player, bool isSuccess, bool isActive)
     {
+        if (isActive)
+        {
+            IsMinigameStarted = true;
 
+            if (!HasMinigameStartedBefore)
+                HasMinigameStartedBefore = true;
+        }
+        else
+        {
+            IsMinigameStarted = false;
+
+        }
     }
 }
