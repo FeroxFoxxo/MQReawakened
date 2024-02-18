1d5460ce66d2656ebd739fe6bd37930d29d7d532
FeroxFoxxo <feroxfoxxo@gmail.com>
2024-02-19 00:42:36 +1300

    Fixed Var's quest

---
+++ Server.Reawakened/Entities/Components/MiniQuestControllerComp.cs
@@ -0,0 +1,27 @@
+﻿using A2m.Server;
+using Server.Reawakened.Players;
+using Server.Reawakened.Players.Extensions;
+using Server.Reawakened.Rooms.Extensions;
+using Server.Reawakened.Rooms.Models.Entities;
+
+namespace Server.Reawakened.Entities.Components;
+
+public class MiniQuestControllerComp : Component<MiniQuestController>
+{
+    public int MaxHit => ComponentData.MaxHit;
+    public float TimeDelay => ComponentData.TimeDelay;
+    public string Endpoint => ComponentData.Endpoint;
+
+    public override void RunSyncedEvent(SyncEvent syncEvent, Player player)
+    {
+        if (syncEvent.Type != SyncEvent.EventType.TriggerReceiver)
+            return;
+
+        var recievedEvent = new TriggerReceiver_SyncEvent(syncEvent);
+
+        player.SendSyncEventToPlayer(syncEvent);
+
+        if (recievedEvent.Activate)
+            player.CheckObjective(ObjectiveEnum.Score, Id, PrefabName, 1);
+    }
+}
---
+++ Server.Reawakened/Entities/Components/TriggerWeatherComp.cs
@@ -0,0 +1,11 @@
+﻿using Server.Reawakened.Entities.AbstractComponents;
+using static TriggerWeather;
+
+namespace Server.Reawakened.Entities.Components;
+
+public class TriggerWeatherComp : TriggerCoopControllerComp<TriggerWeather>
+{
+    public WeatherType WeatherLevel => ComponentData.WeatherLevel;
+    public WeatherGUIEffect ApplyGUIEffect => ComponentData.ApplyGUIEffect;
+    public bool ApplyWeatherToRoom => ComponentData.ApplyWeatherToRoom;
+}
