4c0e9822b038a40284bdf0362bcb00bc5192fa5f
FeroxFoxxo <feroxfoxxo@gmail.com>
2024-02-23 01:38:56 +1300

    Finalised controllers for webplayer client

--- Web.WebPlayer/Controllers/API/GetGame.cs
+++ Web.WebPlayer/Controllers/API/GetGame.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.WebPlayer.Services;
 using FileIO = System.IO.File;
 
 namespace Web.WebPlayer.Controllers.API;
 
 [Route("api/getGame/{gameVersion}")]
-public class GetGame() : Controller
+public class GetGame(LoadGameClients loadGameClients) : Controller
 {
     [HttpGet]
-    public IActionResult GetGameExecutable([FromRoute] string gameVersion)
-    {
-        return Ok();
-        //return new FileContentResult(FileIO.ReadAllBytes(path), "application/octet-stream");
-    }
+    public IActionResult GetGameExecutable([FromRoute] string gameVersion) =>
+        loadGameClients.ClientFiles.TryGetValue(gameVersion, out var path)
+            ? new FileContentResult(FileIO.ReadAllBytes(path), "application/octet-stream")
+            : (IActionResult)new FileNotFoundException();
 }
--- Web.WebPlayer/Controllers/API/GetVersions.cs
+++ Web.WebPlayer/Controllers/API/GetVersions.cs
@@ -1,10 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web.WebPlayer.Configs;
+using Web.WebPlayer.Models;
+using Web.WebPlayer.Services;
 
 namespace Web.WebPlayer.Controllers.API;
 
 [Route("api/getVersions")]
-public class GetVersions() : Controller
+public class GetVersions(LoadGameClients loadGameClients, WebPlayerRwConfig rwConfig) : Controller
 {
     [HttpGet]
-    public IActionResult GetGameVersions() => Ok();
+    public IActionResult GetGameVersions()
+    {
+        var firstVersion = rwConfig.DefaultWebPlayer;
+        var versions = loadGameClients.ClientFiles.Keys.ToArray();
+
+        var versionList = new VersionListModel()
+        {
+            DefaultVersion = firstVersion,
+            Versions = versions
+        };
+
+        return Ok(versionList);
+    }
 }
