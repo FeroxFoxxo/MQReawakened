63d6d8df483ecd2124c5e886e46ade6241789d0c
Zachary Baldwin <zacharybaldwin904@gmail.com>
2024-02-24 19:51:07 -0600

    Fix recipes and daily quests

--- Server.Reawakened/Entities/AbstractComponents/BaseChestControllerComp.cs
+++ Server.Reawakened/Entities/AbstractComponents/BaseChestControllerComp.cs
@@ -16,15 +16,10 @@ public abstract class BaseChestControllerComp<T> : Component<T> where T : BaseCh
         Active = 1
     }
 
-    public bool CanActivateDailies(Player player, string dailyObjectId)
-    {
-        if (player.Character.CurrentCollectedDailies == null)
-            player.Character.CurrentCollectedDailies = [];
-
-        return !player.Character.CurrentCollectedDailies.ContainsKey(dailyObjectId) ||
+    public bool CanActivateDailies(Player player, string dailyObjectId) => 
+        !player.Character.CurrentCollectedDailies.ContainsKey(dailyObjectId) ||
             player.Character.CurrentCollectedDailies.Values.Any(x => x.GameObjectId == dailyObjectId &&
                 x.LevelId == player.Room.LevelInfo.LevelId && DateTime.Now >= x.TimeOfHarvest + TimeSpan.FromDays(1));
-    }
 
     public DailiesModel SetDailyHarvest(string gameObjectId, int levelId, DateTime timeOfHarvest) => new()
     {
