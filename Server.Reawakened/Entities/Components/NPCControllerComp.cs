aeb4b40bee6e03be7018bb504707e0602e4065d6
Zachary Baldwin <zacharybaldwin904@gmail.com>
2024-03-03 04:11:06 -0600

    Add GameVersion for checking previous quests

--- Server.Reawakened/Entities/Components/NPCControllerComp.cs
+++ Server.Reawakened/Entities/Components/NPCControllerComp.cs
@@ -383,61 +383,50 @@ public class NPCControllerComp : Component<NPCController>
         var requiredQuests = QuestCatalog.GetAllQuestLineRequiredQuest(questLine);
         var previousQuests = QuestCatalog.GetListOfPreviousQuests(questData);
 
-        /*
-        var hasFoundRequired = requiredQuests.Count == 0;
+        var canStartQuest = false;
 
-        if (!hasFoundRequired)
+        if (Config.GameVersion >= GameVersion.v2014)
         {
+            canStartQuest = previousQuests.Count == 0;
+
+            foreach (var previousQuest in previousQuests)
+                if (player.Character.Data.CompletedQuests.Contains(previousQuest.Id))
+                {
+                    canStartQuest = true;
+                    break;
+                }
+        }
+        else
+        {
+            canStartQuest = requiredQuests.Count == 0;
+
             foreach (var requiredQuest in requiredQuests)
             {
                 if (player.Character.Data.CompletedQuests.Contains(requiredQuest.Id))
-                {
-                    hasFoundRequired = true;
-                    break;
-                }
+                    foreach (var previousQuest in previousQuests)
+                        if (player.Character.Data.CompletedQuests.Contains(previousQuest.Id))
+                        {
+                            canStartQuest = true;
+                            break;
+                        }
+                break;
             }
         }
 
-        if (hasFoundRequired)
-        {*/
-            var hasFoundPrevious = previousQuests.Count == 0;
-
-            if (!hasFoundPrevious)
-            {
-                foreach (var previousQuest in previousQuests)
-                {
-                    if (player.Character.Data.CompletedQuests.Contains(previousQuest.Id))
-                    {
-                        hasFoundPrevious = true;
-                        break;
-                    }
-                }
-            }
-
-            if (hasFoundPrevious)
-            {
-                Logger.LogDebug("[{QuestName} ({QuestId})] [FOUND QUEST THAT MEETS REQUIREMENTS]", questData.Name, questData.Id);
-                return NPCStatus.QuestAvailable;
-            }
-            else
-            {
-                Logger.LogTrace(
-                    "[{QuestName} ({QuestId})] [DOES NOT MEET PREVIOUS QUESTS] {Quests}",
-                    questData.Name, questData.Id,
-                    string.Join(", ", previousQuests.Select(x => $"{x.Name} ({x.Id})"))
-                );
-            }
-            /*
+        if (canStartQuest)
+        {
+            Logger.LogDebug("[{QuestName} ({QuestId})] [FOUND QUEST THAT MEETS REQUIREMENTS]", questData.Name, questData.Id);
+            return NPCStatus.QuestAvailable;
         }
         else
         {
             Logger.LogTrace(
-                "[{QuestName} ({QuestId})] [DOES NOT MEET REQUIRED QUESTS] {Quests}",
+                "[{QuestName} ({QuestId})] [DOES NOT MEET PREVIOUS QUESTS] {Quests}",
                 questData.Name, questData.Id,
-                string.Join(", ", requiredQuests.Select(x => $"{x.Name} ({x.Id})"))
+                string.Join(", ", previousQuests.Select(x => $"{x.Name} ({x.Id})"))
             );
         }
-        */
+
         return NPCStatus.Unknown;
     }
 
