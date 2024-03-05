1e10b8c7cc05bef28b530abd16c8ae45fbe90d88
Zachary Baldwin <zacharybaldwin904@gmail.com>
2024-03-04 18:34:49 -0600

    Updated game versions

--- Server.Reawakened/Configs/ServerRConfig.cs
+++ Server.Reawakened/Configs/ServerRConfig.cs
@@ -256,7 +256,9 @@ public class ServerRConfig : IRConfig
             { GameVersion.v2014, "boBegnopS_4102_TVE" },
             { GameVersion.vLate2013, "10TNMT_3102_TVE" },
             { GameVersion.vEarly2013, "TNMT_2102_ORP" },
-            { GameVersion.vLate2012, "regnaRrewoP_2102_ORP" }
+            { GameVersion.vLate2012, "TNMT_2102_ORP" },
+            { GameVersion.vEarly2012, "regnaRrewoP_2102_ORP" },
+            { GameVersion.v2011, string.Empty }
         };
 
         CurrentTimedEvent = new Dictionary<GameVersion, string>
@@ -265,7 +267,8 @@ public class ServerRConfig : IRConfig
             { GameVersion.vLate2013, string.Empty },
             { GameVersion.vEarly2013, string.Empty },
             { GameVersion.vLate2012, string.Empty },
-            { GameVersion.vEarly2012, string.Empty }
+            { GameVersion.vEarly2012, string.Empty },
+            { GameVersion.v2011, string.Empty }
         };
     }
 }
