f7b00746145eeb3e3a45b2d2fab1cb05163d0c9e
Zachary Baldwin <zacharybaldwin904@gmail.com>
2024-03-07 20:02:45 -0600

    Move health sync event to be after levelup protocol

--- Server.Reawakened/Rooms/Extensions/PlayerExtensions.cs
+++ Server.Reawakened/Rooms/Extensions/PlayerExtensions.cs
@@ -86,11 +86,11 @@ public static class PlayerExtensions
             IncPowerJewel = player.Character.Data.BadgePoints,
         };
 
-        player.SendSyncEventToPlayer(new Health_SyncEvent(player.GameObjectId.ToString(), player.Room.Time,
-            player.Character.Data.MaxLife, player.Character.Data.MaxLife, player.GameObjectId.ToString()));
-
         foreach (var currentPlayer in player.Room.Players.Values)
             currentPlayer.SendXt("ce", levelUpData, player.UserId);
+
+        player.SendSyncEventToPlayer(new Health_SyncEvent(player.GameObjectId.ToString(), player.Room.Time,
+            player.Character.Data.MaxLife, player.Character.Data.MaxLife, player.GameObjectId.ToString()));
     }
 
     public static void SendStartPlay(this Player player, CharacterModel character,
