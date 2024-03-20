05c392a1fd69cfdc837b0a4ac69d407575261b7f
Zachary Baldwin <zacharybaldwin904@gmail.com>
2024-03-20 02:31:58 -0500

    update RemoveRequest

--- Protocols.External/[f] FriendsHandler/[d] RemoveRequest.cs
+++ Protocols.External/[f] FriendsHandler/[d] RemoveRequest.cs
@@ -8,14 +8,12 @@ public class DeleteResponse : ExternalProtocol
     public override void Run(string[] message)
     {
         var characterName = message[5];
-        var friend = Player.Character.Data.GetFriends().PlayerList.FirstOrDefault(x => x.CharacterName == characterName);
+        var friend = Player.PlayerContainer.GetPlayerByName(characterName);
 
         if (friend != null)
         {
-            var player = Player.PlayerContainer.GetPlayerByName(characterName);
-
             Player.Character.Data.Friends.Remove(friend.CharacterId);
-            player.Character.Data.Friends.Remove(Player.CharacterId);
+            friend.Character.Data.Friends.Remove(Player.CharacterId);
 
             SendXt("fd", characterName, "1");
         }
