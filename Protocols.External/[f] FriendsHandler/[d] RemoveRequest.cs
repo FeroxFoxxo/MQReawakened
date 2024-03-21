3b73c8808ecb7f9dc9cb1fbb2c425a176d900cac
Zachary Baldwin <zacharybaldwin904@gmail.com>
2024-03-20 21:15:32 -0500

    Allow removing offline friends

--- Protocols.External/[f] FriendsHandler/[d] RemoveRequest.cs
+++ Protocols.External/[f] FriendsHandler/[d] RemoveRequest.cs
@@ -1,19 +1,22 @@
 ﻿using Server.Reawakened.Network.Protocols;
+using Server.Reawakened.Players.Services;
 
 namespace Protocols.External._f__FriendsHandler;
 public class DeleteResponse : ExternalProtocol
 {
     public override string ProtocolName => "fd";
 
+    public CharacterHandler CharacterHandler { get; set; }
+
     public override void Run(string[] message)
     {
         var characterName = message[5];
-        var friend = Player.PlayerContainer.GetPlayerByName(characterName);
+        var friend = CharacterHandler.GetCharacterFromName(characterName);
 
         if (friend != null)
         {
-            Player.Character.Data.Friends.Remove(friend.CharacterId);
-            friend.Character.Data.Friends.Remove(Player.CharacterId);
+            Player.Character.Data.Friends.Remove(friend.Id);
+            friend.Data.Friends.Remove(Player.CharacterId);
 
             SendXt("fd", characterName, "1");
         }
