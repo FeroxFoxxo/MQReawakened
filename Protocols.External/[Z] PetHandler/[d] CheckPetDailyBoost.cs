18eb455fdb2744006f81319450e5cd6709ca8d9e
Draykos <Drakebass0830@gmail.com>
2024-03-22 15:33:30 -0400

    Merge Draykos

---
+++ Protocols.External/[Z] PetHandler/[d] CheckPetDailyBoost.cs
@@ -0,0 +1,15 @@
+﻿using Server.Reawakened.Network.Extensions;
+using Server.Reawakened.Network.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protocols.External._Z__PetHandler;
+public class CheckPetDailyBoost : ExternalProtocol
+{
+    public override string ProtocolName => "Zd";
+
+    public override void Run(string[] message) => Player.SendXt("Zd", 0);
+}
