d59dbaff92c2ba0dafa7af686fbedf98aa4a7bb2
monkeyhue <monkeyhuemq@gmail.com>
2024-03-28 16:26:13 -0400

    Fixed enemy hazard bug

--- Server.Reawakened/Entities/Components/EnemyControllerComp.cs
+++ Server.Reawakened/Entities/Components/EnemyControllerComp.cs
@@ -45,8 +45,6 @@ public class EnemyControllerComp : Component<EnemyController>, IDestructible
     public int MaxHealth;
     public int OnKillExp;
 
-    private int _damage;
-
     public override void InitializeComponent()
     {
         Level = Room.LevelInfo.Difficulty + EnemyLevelOffset;
@@ -57,14 +55,6 @@ public class EnemyControllerComp : Component<EnemyController>, IDestructible
 
     public override void NotifyCollision(NotifyCollision_SyncEvent notifyCollisionEvent, Player player)
     {
-        _damage = WorldStatistics.GetValue(ItemEffectType.AbilityPower, WorldStatisticsGroup.Enemy, Level);
-
-        if (_damage < 0)
-            _damage = 1;
-
-        player.ApplyCharacterDamage(_damage, 1, TimerThread);
-
-        return;
     }
 
     public void Damage(int damage, Player origin)
