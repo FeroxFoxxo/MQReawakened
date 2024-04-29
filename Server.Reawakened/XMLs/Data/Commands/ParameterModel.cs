3e11ddbadc8d88b62aaa152e02b56570f174e67e
FeroxFoxxo <feroxfoxxo@gmail.com>
2024-04-30 00:36:57 +1200

    Added more inbuilt commands

--- Server.Reawakened/XMLs/Data/Commands/ParameterModel.cs
+++ Server.Reawakened/XMLs/Data/Commands/ParameterModel.cs
@@ -5,6 +5,7 @@ public class ParameterModel
 {
     public string Name { get; set; }
     public string Description { get; set; }
+    public bool Optional { get; set; }
     public List<OptionModel> Options { get; set; }
 
     public static List<ParameterModel> FromXmlNodes(XmlNodeList nodes)
@@ -12,10 +13,12 @@ public class ParameterModel
         var parameters = new List<ParameterModel>();
         foreach (XmlNode node in nodes)
         {
+            var optional = node.Attributes["optional"];
             parameters.Add(new ParameterModel
             {
                 Name = node.Attributes["name"].Value,
                 Description = node.Attributes["description"].Value,
+                Optional = optional != null && optional.Value == "true",
                 Options = OptionModel.FromXmlNodes(node.SelectNodes("Option"))
             });
         }
