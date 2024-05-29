f185c79de81023141c6e6efaf822a0a6e2a976cd
Draykos <69781246+Draykoss@users.noreply.github.com>
2024-05-28 20:17:58 -0400

    Update PlayerExtensions.cs
    Changed NC reward to use ServerRConfig

--- Server.Reawakened/Players/Extensions/PlayerExtensions.cs
+++ Server.Reawakened/Players/Extensions/PlayerExtensions.cs
@@ -240,14 +240,14 @@ public static class PlayerExtensions
             : string.Empty;
     }
 
-    public static void LevelUp(this Player player, int level, ItemRConfig itemRConfig,
-        ServerRConfig config, Microsoft.Extensions.Logging.ILogger logger)
+    public static void LevelUp(this Player player, int level, 
+    ServerRConfig config, Microsoft.Extensions.Logging.ILogger logger)
     {
         player.Character.SetLevelXp(level, config);
         player.SendLevelUp();
 
         //Temporary NCash reward until original level up system is implemented.
-        player.AddNCash(itemRConfig.LevelUpNCashReward);
+        player.AddNCash(config.LevelUpNCashReward);
         player.SendCashUpdate();
         //(Needed for gameplay improvements as NC is currently unobtainable)
 
