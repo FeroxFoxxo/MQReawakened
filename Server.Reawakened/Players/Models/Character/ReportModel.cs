cabc0270c876b1f1fcfd8c529c644d285abc35b9
Zach <zacharybaldwin904@gmail.com>
2024-07-21 15:11:04 -0500

    Remove ToString method from ReportModel

--- Server.Reawakened/Players/Models/Character/ReportModel.cs
+++ Server.Reawakened/Players/Models/Character/ReportModel.cs
@@ -1,6 +1,4 @@
-﻿using Server.Reawakened.Players.Helpers;
-
-namespace Server.Reawakened.Players.Models.Character;
+﻿namespace Server.Reawakened.Players.Models.Character;
 public class ReportModel
 {
     public Guid Id { get; set; }
@@ -8,17 +6,4 @@ public class ReportModel
     public string Reporter { get; set; }
     public string Reported { get; set; }
     public string Description { get; set; }
-
-    public override string ToString()
-    {
-        var sb = new SeparatedStringBuilder('|');
-
-        sb.Append(Id.ToString());
-        sb.Append(Category);
-        sb.Append(Reporter);
-        sb.Append(Reported);
-        sb.Append(Description);
-
-        return sb.ToString();
-    }
 }
