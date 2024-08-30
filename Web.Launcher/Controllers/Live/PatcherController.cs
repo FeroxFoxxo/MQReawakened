290f2a524e0af57acb442696c5060122d662a161
Zach <zacharybaldwin904@gmail.com>
2024-08-29 22:45:27 -0500

    Add launcher patcher

---
+++ Web.Launcher/Controllers/Live/PatcherController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Launcher.Services;
+
+namespace Web.Launcher.Controllers.Live;
+
+[Route("live/game/win32/{gameVersion}")]
+public class PatcherController(LoadUpdates loadUpdates, ILogger<PatcherController> logger) : Controller
+{
+    [HttpGet]
+    public IActionResult GetFile([FromRoute] string gameVersion)
+    {
+        gameVersion = gameVersion.Replace(".zip", "");
+
+        if (loadUpdates.ClientFiles.TryGetValue(gameVersion, out var path))
+        {
+            var fileBytes = System.IO.File.ReadAllBytes(path);
+
+            logger.LogInformation("Downloading patch version: {GameVersion} at path {Path}", gameVersion, path);
+            return File(fileBytes, "application/zip", gameVersion + ".zip");
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ Web.Launcher/Services/LoadUpdates.cs
@@ -0,0 +1,15 @@
+﻿using Server.Base.Core.Abstractions;
+using Server.Base.Core.Events.Arguments;
+using Server.Base.Core.Events;
+using Web.Launcher.Models;
+
+namespace Web.Launcher.Services;
+public class LoadUpdates(EventSink eventSink, LauncherRConfig rConfig) : IService
+{
+    public Dictionary<string, string> ClientFiles = [];
+
+    public void Initialize() => eventSink.ServerStarted += LoadClients;
+
+    private void LoadClients(ServerStartedEventArgs _) => ClientFiles = Directory.GetFiles(rConfig.GameFolder, "*.zip", SearchOption.AllDirectories)
+            .ToDictionary(Path.GetFileNameWithoutExtension, x => x);
+}
