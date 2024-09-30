6995cc6fc2aee3af5c4970c94020fea552013dbf
feroxfoxxo@gmail.com <feroxfoxxo@gmail.com>
2024-09-30 21:45:11 +1300

    fix: removed extraneous bracket

--- Server.Base/Network/NetState.cs
+++ Server.Base/Network/NetState.cs
@@ -258,7 +258,7 @@ public class NetState : IDisposable
                     {
                         Task.Run(() => RunPacket(packet));
                     }
-                });
+                };
 
                 lock (AsyncLock)
                 {
