9e10e4c6f065f22b1c077b0e0088c4d3ac8872dc
Zach <zacharybaldwin904@gmail.com>
2024-10-29 01:42:07 -0500

    feat: osx patcher support (note: we still don't have any mac client archived)

---
+++ Web.Launcher/Controllers/Live/Win/GamePatcherController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Launcher.Services;
+
+namespace Web.Launcher.Controllers.Live.Win;
+
+[Route("live/game/win32/{gameVersion}")]
+public class GamePatcherController(LoadUpdates loadUpdates, ILogger<GamePatcherController> logger) : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> GetFile([FromRoute] string gameVersion)
+    {
+        gameVersion = gameVersion.Replace(".zip", "");
+
+        if (loadUpdates.WinClientFiles.TryGetValue(gameVersion, out var path))
+        {
+            var memory = new MemoryStream();
+
+            using (var stream = new FileStream(path, FileMode.Open))
+                await stream.CopyToAsync(memory);
+
+            memory.Position = 0;
+
+            logger.LogInformation("Downloading patch version: {GameVersion} at path {Path}", gameVersion, path);
+            return File(memory, "application/octet-stream", gameVersion + ".zip");
+        }
+        else
+            return NotFound();
+    }
+}
---
+++ Web.Launcher/Controllers/Live/Win/LauncherPatcherController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Launcher.Services;
+
+namespace Web.Launcher.Controllers.Live.Win;
+
+[Route("live/launcher/win32/{launcherVersion}")]
+public class LauncherPatcherController(LoadUpdates loadUpdates, ILogger<LauncherPatcherController> logger) : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> GetFile([FromRoute] string launcherVersion)
+    {
+        launcherVersion = launcherVersion.Replace(".zip", "");
+
+        if (loadUpdates.WinLauncherFiles.TryGetValue(launcherVersion, out var path))
+        {
+            var memory = new MemoryStream();
+
+            using (var stream = new FileStream(path, FileMode.Open))
+                await stream.CopyToAsync(memory);
+
+            memory.Position = 0;
+
+            logger.LogInformation("Downloading patch version: {GameVersion} at path {Path}", launcherVersion, path);
+            return File(memory, "application/octet-stream", launcherVersion + ".zip");
+        }
+        else
+            return NotFound();
+    }
+}
