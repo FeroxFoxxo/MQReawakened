a8516e3d97eb7b54157434dff93098016bc118cd
Zach <zacharybaldwin904@gmail.com>
2025-01-26 20:56:05 -0600

    fix: 2012 and early 2013 event prefab errors

--- Server.Reawakened/XMLs/Bundles/Base/EventPrefabs.cs
+++ Server.Reawakened/XMLs/Bundles/Base/EventPrefabs.cs
@@ -95,10 +95,59 @@ public class EventPrefabs : EventPrefabsXML, IBundledXml
                 {
                     case "noitarbeleC_enuJ_10R":
                     case "htnoM21_pihsrebmeM_TVE":
+                    case "adnaPuFgnuK_2102_ORP":
+                    case "yrasrevinnA_2102_TVE":
+                    case "steP_2102_TVE":
+                    case "semaGiniM_2102_TVE":
+                    case "10_steP_CN_3102_TVE":
+                    case "konhcaR_3102_TVE":
+                    case "nonnaCittefnoC_3102_TVE":
+                    case "adnaP_steP_3102_TVE":
+                    case "20_steP_CN_3102_TVE":
+                    case "wen20_steP_CN_3102_TVE":
+                    case "snruteR_ekirtsrebaS_3102_TVE":
+                    case "10reppaZnekcihC_3102_TVE":
+                    case "10_elaS_steP_CN_3102_TVE":
+                    case "neewollaH_3102_TVE":
+                    case "10yadiloH_3102_TVE":
+                    case "weiverPSTC_3102_TVE":
+                    case "20weiverPSTC_3102_TVE":
+                    case "10tePyraG_3102_TVE":
+                    case "10_3102boBegnopS_3102_TVE":
+                    case "20_3102boBegnopS_3102_TVE":
+                    case "30_3102boBegnopS_3102_TVE":
+                    case "40_3102boBegnopS_3102_TVE":
+                    case "50_3102boBegnopS_3102_TVE":
                         parent.RemoveChild(events);
                         break;
                     default:
-                        if (eventName.Contains("1102") || eventName.Contains("2102"))
+                        if (eventName.Contains("1102"))
+                            parent.RemoveChild(events);
+                        break;
+                }
+            }
+        }
+        else if (RConfig.GameVersion >= GameVersion.vEarly2012)
+        {
+            var node = xml.SelectNodes("/events/event");
+
+            foreach (XmlNode events in node)
+            {
+                var parent = events.ParentNode;
+                var eventName = new string(events.Attributes["name"].Value.Reverse().ToArray());
+
+                switch (eventName)
+                {
+                    case "noitarbeleC_enuJ_10R":
+                    case "htnoM21_pihsrebmeM_TVE":
+                    case "adnaPuFgnuK_2102_ORP":
+                    case "yrasrevinnA_2102_TVE":
+                    case "steP_2102_TVE":
+                    case "semaGiniM_2102_TVE":
+                        parent.RemoveChild(events);
+                        break;
+                    default:
+                        if (eventName.Contains("1102"))
                             parent.RemoveChild(events);
                         break;
                 }
