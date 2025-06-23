abd8e8c308caf2ae5df3c13c6e911ceaeaf5f9ac
FeroxFoxxo <feroxfoxxo@gmail.com>
2025-06-24 11:20:51 +1200

    chore: removed checks forai properties

--- Server.Reawakened/XMLs/Data/Enemy/Models/EnemyModel.cs
+++ Server.Reawakened/XMLs/Data/Enemy/Models/EnemyModel.cs
@@ -22,45 +22,5 @@ public class EnemyModel
             logger.LogError("Enemy '{Name}' does not have a loot table attached!", enemyType);
             EnemyLootTable = [];
         }
-
-        if (Hitbox == null)
-        {
-            logger.LogError("Enemy '{Name}' does not have a hit box attached!", enemyType);
-            Hitbox = new HitboxModel(1, 1, 0.5f, 0.5f);
-        }
-
-        ComponentOverrides ??= [];
-
-        switch(AiType)
-        {
-            case AiType.Behavior:
-                if (BehaviorData == null)
-                {
-                    logger.LogError("Enemy '{Name}' does not have a behavior data attached!", enemyType);
-                    BehaviorData = [];
-                }
-
-                if (GlobalProperties == null)
-                {
-                    logger.LogError("Enemy '{Name}' does not have any global properties attached!", enemyType);
-                    GlobalProperties = new GlobalPropertyModel(false, 0, 0, 0, 0, 0, 0, 0, 0, 0, string.Empty, "COL_PRJ_DamageProjectile", false, false, 0);
-                }
-
-                if (GenericScript == null)
-                {
-                    logger.LogError("Enemy '{Name}' does not have any generic script attached!", enemyType);
-                    GenericScript = new GenericScriptModel(StateType.Unknown, StateType.Unknown, StateType.Unknown, 0, 0, 0);
-                }
-
-                if (!BehaviorData.ContainsKey(GenericScript.AttackBehavior))
-                    logger.LogError("Enemy '{Name}' does not have the attack behavior '{Behavior}' defined!", enemyType, GenericScript.AttackBehavior);
-
-                if (!BehaviorData.ContainsKey(GenericScript.AwareBehavior))
-                    logger.LogError("Enemy '{Name}' does not have the aware behavior '{Behavior}' defined!", enemyType, GenericScript.AwareBehavior);
-
-                if (!BehaviorData.ContainsKey(GenericScript.UnawareBehavior))
-                    logger.LogError("Enemy '{Name}' does not have the unaware behavior '{Behavior}' defined!", enemyType, GenericScript.UnawareBehavior);
-                break;
-        }
     }
 }
