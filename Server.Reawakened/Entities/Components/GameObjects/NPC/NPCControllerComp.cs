b994b57cb3db16b0dce118ce82cb1f4f6e47397a
Zach <zacharybaldwin904@gmail.com>
2025-08-31 17:46:09 -0500

    fix: ice raider tutorial quest version check

--- Server.Reawakened/Entities/Components/GameObjects/NPC/NPCControllerComp.cs
+++ Server.Reawakened/Entities/Components/GameObjects/NPC/NPCControllerComp.cs
@@ -417,7 +417,7 @@ public class NPCControllerComp : Component<NPCController>
                 return NPCStatus.Unknown;
             }
 
-        if (Config.GameVersion < GameVersion.vEarly2013 && questData.Name == "T4IR_00_01" 
+        if (Config.GameVersion < GameVersion.vEarly2014 && questData.Name == "T4IR_00_01" 
             && !player.Character.CompletedQuests.Contains(939))
         {
             Logger.LogTrace("[{QuestName}] ({QuestId}) [SKIPPED QUEST] Not all tribe tutorial quests are completed.", questData.Name, questData.Id);
