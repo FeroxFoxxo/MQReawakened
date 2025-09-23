e0ab829823bf2b7943b0420d19e5f902a77fb19f
Zach <zacharybaldwin904@gmail.com>
2025-09-23 16:40:15 -0500

    fix: ice raider arenas

--- Server.Reawakened/Entities/Components/GameObjects/Spawners/BaseSpawnerControllerComp.cs
+++ Server.Reawakened/Entities/Components/GameObjects/Spawners/BaseSpawnerControllerComp.cs
@@ -5,7 +5,6 @@ using Server.Base.Timers.Extensions;
 using Server.Base.Timers.Services;
 using Server.Reawakened.Entities.Components.AI.Stats;
 using Server.Reawakened.Entities.Components.GameObjects.Breakables;
-using Server.Reawakened.Entities.Components.GameObjects.Hazards;
 using Server.Reawakened.Entities.Components.GameObjects.Trigger;
 using Server.Reawakened.Entities.Components.GameObjects.Trigger.Enums;
 using Server.Reawakened.Entities.Enemies.Behaviors.Abstractions;
@@ -19,7 +18,6 @@ using Server.Reawakened.XMLs.Bundles.Internal;
 using Server.Reawakened.XMLs.Data.Enemy.Abstractions;
 using Server.Reawakened.XMLs.Data.Enemy.Enums;
 using Server.Reawakened.XMLs.Data.Enemy.Models;
-using Server.Reawakened.XMLs.Data.Enemy.States;
 using UnityEngine;
 
 namespace Server.Reawakened.Entities.Components.GameObjects.Spawners;
@@ -257,8 +255,11 @@ public class BaseSpawnerControllerComp : Component<BaseSpawnerController>
         Logger.LogInformation("Spawner '{Id}' spawning enemy #{Num} prefab '{Prefab}' template '{Template}'", Id, _spawnedEntityCount, selectedPrefab, templateId);
 
         // Make sure to modify patrol data before init to account for patrol range
-        genericComp.PatrolX = PatrolDistance.x;
-        genericComp.PatrolY = PatrolDistance.y;
+        if (genericComp != null)
+        {
+            genericComp.PatrolX = PatrolDistance.x;
+            genericComp.PatrolY = PatrolDistance.y;
+        }
 
         // Init event must be called before AIDo or else the enemy freezes
         Room.SendSyncEvent(
