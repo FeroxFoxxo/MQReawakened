275002710f5892a1552370c51e29489f949ccec4
Zach <zacharybaldwin904@gmail.com>
2025-12-24 01:22:43 -0600

    fix: guardian crests not working

--- Server.Reawakened/Entities/Projectiles/ChargeAttackProjectile.cs
+++ Server.Reawakened/Entities/Projectiles/ChargeAttackProjectile.cs
@@ -33,7 +33,7 @@ public class ChargeAttackProjectile : BaseProjectile
         Collider = new AttackCollider(id, Position, new RectModel(-0.4f, -0.5f, 0.8f, 1), PrjPlane, player, damage, type, 15f, 0, player.Character.StatusEffects.HasEffect(ItemEffectType.Detect));
 
         Room.SendSyncEvent(new ChargeAttackStart_SyncEvent(player.GameObjectId.ToString(), Room.Time,
-                        endPosition.x, endPosition.y, speed.x, speed.y, _itemId, -1));
+                        endPosition.x, endPosition.y, speed.x, speed.y, _itemId, zoneId));
     }
 
     public override void Hit(string hitGoID)
@@ -47,7 +47,7 @@ public class ChargeAttackProjectile : BaseProjectile
         Room.SendSyncEvent(
             new ChargeAttackStop_SyncEvent(
                 _player.GameObjectId.ToString(), Room.Time,
-                _player.TempData.Position.X, _player.TempData.Position.Y, _itemId, -1, hitGoID
+                _player.TempData.Position.X, _player.TempData.Position.Y, _itemId, _zoneId, hitGoID
             )
         );
 
