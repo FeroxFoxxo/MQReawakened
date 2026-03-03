37c3f7d069a1563e2ee446d147748c27061a9950
Zach <zacharybaldwin904@gmail.com>
2026-03-03 04:16:42 -0600

    fix: monkey blast best distance

---
+++ Protocols.External/[M] MinigameHandler/[D] GetDistance.cs
@@ -0,0 +1,27 @@
+﻿using Server.Reawakened.Network.Extensions;
+using Server.Reawakened.Network.Protocols;
+using Web.Apps.Leaderboards.Database.Scores;
+
+namespace Protocols.External._M__MinigameHandler;
+public class GetDistance : ExternalProtocol
+{
+    public override string ProtocolName => "MD";
+
+    public TopScoresHandler TopScoresHandler { get; set; }
+
+    public override void Run(string[] message)
+    {
+        var monkeyBlastId = 5;
+        var topScores = TopScoresHandler.GetScoresFromId(monkeyBlastId);
+
+        if (topScores != null && topScores.Scores.Count > 0)
+        {
+            if (topScores.Scores.Any(x => x.CharacterId == Player.Character.Id))
+            {
+                var score = topScores.Scores.FirstOrDefault(x => x.CharacterId == Player.Character.Id);
+
+                Player.SendXt("MD", (long)score.Score);
+            }
+        }
+    }
+}
